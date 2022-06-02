fdbaef42c8209ab861318e8cb4be87b400f71c62
Michael Matloka <dev@twixes.com>
2022-06-02 16:00:49 +0200

    Use `node` env in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+    env: {
+        node: true,
+    },
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     extends: ['plugin:@typescript-eslint/recommended'],
